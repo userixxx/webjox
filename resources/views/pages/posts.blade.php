ff930180e5f415ee693a6caf0e27c702629ee54a
userixxx <131803136+userixxx@users.noreply.github.com>
2024-02-02 02:55:52 +0300

    Update posts.blade.php

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -1,11 +1,25 @@
-<!-- resources/views/posts.blade.php -->
-
 @extends('layouts.app')
 
 @section('title')Все посты@endsection
 
 @section('content')
     <h1>Посты</h1>
+
+    <form action="{{ route('posts.index') }}" method="GET" class="mt-3">
+        @csrf
+        <div class="mb-3">
+            <label for="category" class="form-label">Отфильтровать?</label>
+            <select name="category" id="category" class="form-select">
+                <option value="">Все категории</option>
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->id }}</option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Применить</button>
+    </form>
+
+
     <div class="posts">
         <div class="row">
             @foreach($posts as $post)
